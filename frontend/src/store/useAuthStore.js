18969269b5fc5795a8c14f31b2185241acf17686
Victor <victorshek412@gmail.com>
2026-02-23 05:00:41 +0800

    fix: correct isLoading property casing and update login state management

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,10 +3,10 @@ import { create } from "zustand";
 export const useAuthStore = create((set) => ({
   authUser: { name: "Victor", _id: 123, age: 18 },
   isLoggedIn: false,
-  isloading: false,
+  isLoading: false,
 
   login: (user) => {
     console.log("We just logged in");
-    set({ authUser: user, isLoggedIn: true });
+    set({ authUser: user, isLoggedIn: true, isLoading: true });
   },
 }));
